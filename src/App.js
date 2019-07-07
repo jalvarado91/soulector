44dd14f4417cbf0e7e1301137abff0f282b58398
Juan Alvarado <jalvarado91@gmail.com>
2019-07-07 17:52:17 -0400

    change proxy setup to account for redirects on netlify

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class LambdaDemo extends Component {
     e.preventDefault()
 
     this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
+    fetch(api)
       .then(response => response.json())
       .then(json => this.setState({ loading: false, msg: json.msg }))
   }
