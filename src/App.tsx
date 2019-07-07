e18c2a8cc8a8fcb049656c3a2db1a167561b4790
Juan Alvarado <jalvarado91@gmail.com>
2019-07-07 18:55:52 -0400

    splash

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,11 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-
-class LambdaDemo extends Component<
-  {},
-  { loading: boolean; msg: string | null }
-> {
-  constructor(props: any) {
-    super(props);
-    this.state = { loading: false, msg: null };
-  }
-
-  handleClick = (api: string) => (e: React.MouseEvent) => {
-    e.preventDefault();
-
-    this.setState({ loading: true });
-    fetch(api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }));
-  };
-
-  render() {
-    const { loading, msg } = this.state;
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>
-          {loading ? "Loading..." : "Call Lambda"}
-        </button>
-        <button onClick={this.handleClick("async-dadjoke")}>
-          {loading ? "Loading..." : "Call Async Lambda"}
-        </button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    );
-  }
-}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
+        <h1>Soulector</h1>
       </div>
     );
   }
