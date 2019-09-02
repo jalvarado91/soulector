df8cc15f2f22692afdf0d95e37afebfc7f0a6bba
Juan Alvarado <jalvarado91@gmail.com>
2019-09-02 01:59:54 -0400

    virtualized window

--- src/components/Tracks.tsx
+++ src/components/Tracks.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { ITrack } from "../types";
 import { Track } from "./Track";
 import { TracksStateContext } from "../context/TracksContext";
+import { FixedSizeList as List } from "react-window";
 
 export function Tracks() {
   const { tracks } = useContext(TracksStateContext);
@@ -21,12 +22,29 @@ export function TracksView(props: TracksProps) {
   const playingTrack = props.tracks.length > 0 && props.tracks[4]._id;
 
   return (
-    <div className="w-full max-w-4xl mx-auto px-3 md:px-4 py-8">
-      {props.tracks.map(track => (
-        <div key={track._id} className="">
-          <Track track={track} playing={track._id === playingTrack} />
-        </div>
-      ))}
+    <div className="w-full flex justify-center">
+      {props.tracks.length > 0 ? (
+        <List
+          className="px-4 md:px-4"
+          height={window.innerHeight}
+          itemCount={props.tracks.length}
+          itemSize={90}
+          width={"100%"}
+        >
+          {({ index, style }) => {
+            const track = props.tracks[index];
+            return (
+              <div className="w-full" style={style}>
+                <div className="max-w-4xl m-auto">
+                  <Track track={track} playing={track._id === playingTrack} />
+                </div>
+              </div>
+            );
+          }}
+        </List>
+      ) : (
+        <p>loading...</p>
+      )}
     </div>
   );
 }
