1d15df1f428cc8da3a3701ca521c62c8de8f7b8f
Juan Alvarado <jalvarado91@gmail.com>
2019-10-30 12:45:41 -0400

    Autosizer on tracks list and nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./index.css";
 import { TracksContextController } from "./context/TracksContext";
 import { Tracks } from "./components/Tracks";
+import { Soulector } from "./components/Icons";
 
 export function App() {
   return (
@@ -10,7 +11,13 @@ export function App() {
         <h1 className="text-3xl font-bold">Soulector</h1>
       </div>
       <hr /> */}
-      <Tracks />
+      <div className="flex flex-col h-screen text-gray-900">
+        <div className="px-6 py-3 shadow-md flex">
+          <Soulector className="w-8 mr-2" />
+          <div className="text-2xl font-bold">Soulector</div>
+        </div>
+        <Tracks />
+      </div>
     </TracksContextController>
   );
 }
--- src/components/Tracks.tsx
+++ src/components/Tracks.tsx
@@ -3,14 +3,15 @@ import { ITrack } from "../types";
 import { Track } from "./Track";
 import { TracksStateContext } from "../context/TracksContext";
 import { FixedSizeList as List } from "react-window";
+import AutoSizer from "react-virtualized-auto-sizer";
 
 export function Tracks() {
   const { tracks, loading } = useContext(TracksStateContext);
 
   return (
-    <div>
+    <React.Fragment>
       {loading ? "loading from reducer" : <TracksView tracks={tracks} />}
-    </div>
+    </React.Fragment>
   );
 }
 
@@ -22,28 +23,35 @@ export function TracksView(props: TracksProps) {
   const playingTrack = props.tracks.length > 0 && props.tracks[4]._id;
 
   return (
-    <div className="w-full flex justify-center">
-      {props.tracks.length > 0 ? (
-        <List
-          className="px-4 md:px-4"
-          height={window.innerHeight}
-          itemCount={props.tracks.length}
-          itemSize={90}
-          width={"100%"}
-        >
-          {({ index, style }) => {
-            const track = props.tracks[index];
-            return (
-              <div className="w-full" style={style}>
-                <div className="max-w-4xl m-auto">
-                  <Track track={track} playing={track._id === playingTrack} />
-                </div>
-              </div>
-            );
-          }}
-        </List>
-      ) : (
-        <p>loading...</p>
+    <div className="flex h-full justify-center">
+      {props.tracks.length > 0 && (
+        <div className="flex-auto">
+          <AutoSizer>
+            {({ height, width }) => (
+              <List
+                className="px-4 md:px-4"
+                itemCount={props.tracks.length}
+                itemSize={90}
+                width={width}
+                height={height}
+              >
+                {({ index, style }) => {
+                  const track = props.tracks[index];
+                  return (
+                    <div className="w-full" style={style}>
+                      <div className="max-w-4xl m-auto">
+                        <Track
+                          track={track}
+                          playing={track._id === playingTrack}
+                        />
+                      </div>
+                    </div>
+                  );
+                }}
+              </List>
+            )}
+          </AutoSizer>
+        </div>
       )}
     </div>
   );
