4e51c8a5e29b78605420a5f4d49a73c8c97eb8fc
Juan Alvarado <jalvarado91@gmail.com>
2019-12-29 20:39:03 -0500

    Content before virtualized list container

--- src/components/Tracks.tsx
+++ src/components/Tracks.tsx
@@ -1,4 +1,10 @@
-import React, { useContext, useEffect, useRef } from "react";
+import React, {
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+  useLayoutEffect
+} from "react";
 import { ITrack } from "../types";
 import { Track } from "./Track";
 import { TracksStateContext } from "../context/TracksContext";
@@ -11,6 +17,7 @@ import {
 import { cx } from "emotion";
 import { ShuffleIcon, Soulector } from "./Icons";
 import { sample } from "lodash-es";
+import { min } from "date-fns/esm";
 
 export function TracksContainer() {
   const { currentTrackId } = useContext(PlayerContextState);
@@ -67,6 +74,18 @@ function TracksLoading() {
   );
 }
 
+type BeforeListProps = {
+  numTracks: number;
+};
+function BeforeList({ numTracks }: BeforeListProps) {
+  return (
+    <div className="px-4 flex item-center mt-4 mb-2">
+      <div className="font-semibold mr-auto text-indigo-900">All Episodes</div>
+      <div className="font-semibold text-gray-600">{numTracks} Total</div>
+    </div>
+  );
+}
+
 type TracksProps = {
   tracks: ITrack[];
   currentTrackId?: string;
@@ -84,6 +103,15 @@ export function Tracks({
 }: TracksProps) {
   const listRef = useRef<List>(null);
 
+  const beforeListRef = useRef<HTMLDivElement | null>(null);
+  const [beforeListHight, setBeforeListHeight] = useState(-1);
+  useLayoutEffect(() => {
+    if (beforeListRef.current) {
+      const domHeight = beforeListRef.current.getBoundingClientRect().height;
+      setBeforeListHeight(domHeight);
+    }
+  }, [beforeListHight]);
+
   useEffect(() => {
     if (listRef.current) {
       const trackIdx = tracks.findIndex(t => t._id === focusTrackId);
@@ -93,7 +121,13 @@ export function Tracks({
     }
   }, [focusTrackId, tracks]);
 
-  return (
+  // Render an invisible version of the content before element
+  // in order to measure it's height and render the right virtualized list
+  return beforeListHight < 0 ? (
+    <div className="opacity-0 border" ref={beforeListRef}>
+      <BeforeList numTracks={tracks.length} />
+    </div>
+  ) : (
     <div className="flex h-full justify-center border-2 relative">
       <div className="absolute border-blue-500 right-0 bottom-0 mb-5 mr-5 z-10">
         <button
@@ -121,12 +155,30 @@ export function Tracks({
                 itemSize={90}
                 width={width}
                 height={height}
+                key={beforeListHight}
               >
                 {({ index, style }) => {
                   const track = tracks[index];
+                  const top = style.top as string;
+                  const fHeight =
+                    parseFloat(style.height as string) + beforeListHight;
                   return (
-                    <div className="w-full" style={style}>
+                    <div
+                      className="w-full"
+                      style={{
+                        ...style,
+                        ...(index !== 0 && {
+                          top: `${parseFloat(top) + beforeListHight}px`
+                        }),
+                        ...(index === 0 && { height: fHeight })
+                      }}
+                    >
                       <div className="max-w-4xl m-auto">
+                        {index === 0 && (
+                          <div>
+                            <BeforeList numTracks={tracks.length} />
+                          </div>
+                        )}
                         <Track
                           onClick={() => onTrackClick(track._id)}
                           track={track}
